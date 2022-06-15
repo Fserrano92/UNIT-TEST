8a5832c6f8ee17adf59391178504ecb2c8155064
fabian serrano <fabian.serrano.92@gmail.com>
2022-06-15 08:42:02 -0600

    adicion de lumbral de aceptacion

--- karma.conf.js
+++ karma.conf.js
@@ -31,7 +31,15 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 50,
+          branches: 40,
+          functions: 50,
+          lines: 50
+        }
+      }
     },
     reporters: ['mocha'],
     port: 9876,
